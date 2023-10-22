56259c4a28f9ba4c41c6f8e37ab3c589b4898d67
Liliya <liliya.penza@gmail.com>
2023-10-22 12:14:46 +0200

    test

---
+++ src/test/java/ru/netology/MainTest.java
@@ -0,0 +1,28 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void main() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("anything") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("anything"))
+        ;
+    }
+}
